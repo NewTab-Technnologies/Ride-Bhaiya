afb322b363cdfa1b33b472486edf2ebdaee63b68
akshaya-527 <158996512+akshaya-527@users.noreply.github.com>
2024-05-17 13:38:33 +0530

    splash screen
    * splash screen updated
    
    * changed test files
    
    ---------
    
    Co-authored-by: Mahesh Alyana <maheshalyana@Mahesh-MacBook-Air.local>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ridebhaiya/screens/splash_screen.dart';
+import 'package:ridebhaiya/screens/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,12 +13,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RideBhaiya',
+      
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF49B6F3)),
+       
         useMaterial3: true,
       ),
       home:
           const SplashScreen(), // when you change this class name in local, you need to revert it back to SplashScreen() before pushing to GitHub
+      // when you change this class name in local, you need to revert it back to SplashScreen() before pushing to GitHub
     );
   }
 }
