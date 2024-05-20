4a6178cdaf1322843a2d36d581a2d7ef68096d27
pradeepongole2005@gmail.com <pradeepongole2005@gmail.com>
2024-05-21 01:51:40 +0530

    Updated style format

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ridebhaiya/screens/splash_screen.dart';
 
-void main() 
-{
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget 
-{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
--- lib/widgets/request_ride_schedule_tile.dart
+++ lib/widgets/request_ride_schedule_tile.dart
@@ -1,93 +1,88 @@
 import 'package:flutter/material.dart';
 
-class ScheduleTile extends StatefulWidget 
-{
-  const ScheduleTile ({super.key});
+class ScheduleTile extends StatefulWidget {
+  const ScheduleTile({super.key});
 
   @override
   State<ScheduleTile> createState() => _ScheduleTileState();
 }
 
-class _ScheduleTileState extends State<ScheduleTile> 
-{
-  List <String> parameters = ["Name", "From", "To", "Date", "Time", "Seats", "Contact"];
-  List <String> values = ["Venkat Sai", "Dayton", "Denver Airport", "24th April 2024", "4:00 AM", "2", "(+1) 7203000803"];
+class _ScheduleTileState extends State<ScheduleTile> {
+  List<String> parameters = [
+    "Name",
+    "From",
+    "To",
+    "Date",
+    "Time",
+    "Seats",
+    "Contact"
+  ];
+  List<String> values = [
+    "Venkat Sai",
+    "Dayton",
+    "Denver Airport",
+    "24th April 2024",
+    "4:00 AM",
+    "2",
+    "(+1) 7203000803"
+  ];
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return Scaffold
-    (
-      backgroundColor: const Color.fromRGBO(73, 182, 243, 1.000),
-      body: Row
-      (
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: 
-        [
-          Column
-          (
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: 
-            [
-              const SizedBox(height: 173,),
-              TileTwoButtons(parameters: parameters, values: values),                                                   
-            ],
-          ),
-        ],
-      )
-    );
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: const Color.fromRGBO(73, 182, 243, 1.000),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 173,
+                ),
+                TileTwoButtons(parameters: parameters, values: values),
+              ],
+            ),
+          ],
+        ));
   }
 }
 
-class TileTwoButtons extends StatelessWidget 
-{
-  const TileTwoButtons
-  (
-    {super.key, required this.parameters, required this.values,}
-  );
+class TileTwoButtons extends StatelessWidget {
+  const TileTwoButtons({
+    super.key,
+    required this.parameters,
+    required this.values,
+  });
 
   final List<String> parameters;
   final List<String> values;
 
   @override
-  Widget build(BuildContext context) 
-  {
-    return Container
-    (
-      decoration: BoxDecoration
-      (
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
         color: const Color.fromRGBO(255, 255, 255, 1.000),
         borderRadius: BorderRadius.circular(28.0),
       ),
-      
-      height: 328.0, width: 326.0,
-      child: Column
-      (
-        children: 
-        [
-          Padding
-          (
+      height: 328.0,
+      width: 326.0,
+      child: Column(
+        children: [
+          Padding(
             padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-            child: Row
-            (
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>
-              [
-                Column
-                (
-                  children: parameters.map((p)
-                  {
-                    return 
-                    Padding
-                    (
+              children: <Widget>[
+                Column(
+                  children: parameters.map((p) {
+                    return Padding(
                       padding: const EdgeInsets.only(bottom: 6.0),
-                      child: Text
-                      (
+                      child: Text(
                         p,
-                        style: const TextStyle
-                        (
-                          color: Color.fromRGBO(73,182,243, 1.000),
+                        style: const TextStyle(
+                          color: Color.fromRGBO(73, 182, 243, 1.000),
                           fontFamily: "Poppins",
                           fontWeight: FontWeight.w400,
                         ),
@@ -95,19 +90,14 @@ class TileTwoButtons extends StatelessWidget
                     );
                   }).toList(),
                 ),
-                Column
-                (
-                  children: values.map((v)
-                  {
-                    return Padding
-                    (
+                Column(
+                  children: values.map((v) {
+                    return Padding(
                       padding: const EdgeInsets.only(bottom: 6.0),
-                      child: Text
-                      (
+                      child: Text(
                         v,
-                        style: const TextStyle
-                        (
-                          color: Color.fromRGBO(73,182,243, 1.000),
+                        style: const TextStyle(
+                          color: Color.fromRGBO(73, 182, 243, 1.000),
                           fontFamily: "Poppins",
                           fontWeight: FontWeight.w400,
                         ),
@@ -119,20 +109,19 @@ class TileTwoButtons extends StatelessWidget
             ),
           ),
           const SizedBox(height: 10.0),
-          TextButton
-          (
+          TextButton(
             key: const Key("contactButton"),
-            onPressed:(){}, 
-            style: const ButtonStyle
-            (
-              backgroundColor: MaterialStatePropertyAll(Color.fromRGBO(73,182,243, 1.000)),
-              fixedSize: MaterialStatePropertyAll(Size(164.1, 32.63),),
+            onPressed: () {},
+            style: const ButtonStyle(
+              backgroundColor:
+                  MaterialStatePropertyAll(Color.fromRGBO(73, 182, 243, 1.000)),
+              fixedSize: MaterialStatePropertyAll(
+                Size(164.1, 32.63),
+              ),
             ),
-            child: const Text
-            (
+            child: const Text(
               "Contact",
-              style: TextStyle
-              (
+              style: TextStyle(
                 color: Color.fromRGBO(255, 255, 255, 1.000),
                 fontFamily: "Poppins",
                 fontWeight: FontWeight.w400,
@@ -140,21 +129,19 @@ class TileTwoButtons extends StatelessWidget
             ),
           ),
           const SizedBox(height: 9.74),
-          TextButton
-          (
+          TextButton(
             key: const Key("viewMoreButton"),
             onPressed: () {},
-            style: ButtonStyle
-            (
-              backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(73,182,243, 1.000),),
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                const Color.fromRGBO(73, 182, 243, 1.000),
+              ),
               fixedSize: MaterialStateProperty.all(const Size(164.1, 32.63)),
             ),
-            child: const Text
-            (
+            child: const Text(
               "View More",
-              style: TextStyle
-              (
-                color: Color.fromRGBO(255,255,255,1.000),
+              style: TextStyle(
+                color: Color.fromRGBO(255, 255, 255, 1.000),
                 fontFamily: "Poppins",
                 fontWeight: FontWeight.w400,
               ),
