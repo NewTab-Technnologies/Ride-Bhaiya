0e52cc4722c67a0420dee787863fec9c2abc1753
Akshaya Mudam <akshayamudam@gmail.com>
2024-05-22 16:17:26 +0530

    [FEATURE] Implemented Schedule/Request Ride screen

---
+++ test/schedule_test.dart
@@ -0,0 +1,24 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ridebhaiya/screens/schedule_screen.dart';
+
+void main() {
+  testWidgets('ScheduleRideForm test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: ScheduleRideScreen()));
+
+    expect(find.text('RIDE BHAIYA'), findsOneWidget);
+    expect(find.text('Schedule a Ride'), findsOneWidget);
+    expect(find.text('Schedule'), findsOneWidget);
+    expect(find.byIcon(Icons.access_time), findsOneWidget);
+
+    await tester.tap(find.byType(ElevatedButton));
+    await tester.pump();
+  });
+}
