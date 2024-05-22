4141121dcaed89d143a2eb8278b3ad3171beb5e5
Tanishq Sai Govind <tanishqsaigovind@gmail.com>
2024-05-22 15:42:59 +0530

    [FEATURE] Implemeted Get OTP screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'RideBhaiya',
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF49B6F3)),
         useMaterial3: true,
